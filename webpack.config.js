5473c6a3191ecb3a6000eb2ff81596972f07b982
Rodvin Danaei <rodvin@Rodvins-MBP.hitronhub.home>
2018-07-20 11:18:42 -0600

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,95 @@
 const path = require('path');
+//const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  //const CSSExtract = new ExtractTextPlugin('styles.css');
+  const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' });
+  console.log('env', env);
+  return {
     entry: './src/app.js',
     output: {
-        path: path.join(__dirname, 'public'),
-        filename:'bundle.js'
+      path: path.join(__dirname, 'public'),
+      filename:'bundle.js'
     },
-    module: {
-        rules:[{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
+    module: 
+    {
+      rules:
+      [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        }, 
+        {
+          test: /\.s?css$/,
             use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
+              MiniCssExtractPlugin.loader, 
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true
+                }
+              }
             ]
-        }]
+          
+          
+          // use: CSSExtract.extract({
+          //   use: [
+          //     'css-loader',
+          //     'sass-loader'
+          //   ]
+          // })
+          // use: [
+          //   'style-loader',
+          //   'css-loader',
+          //   'sass-loader'
+          // ]
+        }
+    ]
     },
-    devtool: 'cheap-module-eval-source-map',
+    plugins: [
+      CSSExtract
+    ],
+    //devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
     }
+  };
 };
+
+// module.exports = {
+//     entry: './src/app.js',
+//     output: {
+//         path: path.join(__dirname, 'public'),
+//         filename:'bundle.js'
+//     },
+//     module: {
+//         rules:[{
+//             loader: 'babel-loader',
+//             test: /\.js$/,
+//             exclude: /node_modules/
+//         }, {
+//             test: /\.s?css$/,
+//             use: [
+//                 'style-loader',
+//                 'css-loader',
+//                 'sass-loader'
+//             ]
+//         }]
+//     },
+//     devtool: 'cheap-module-eval-source-map',
+//     devServer: {
+//         contentBase: path.join(__dirname, 'public'),
+//         historyApiFallback: true
+//     }
+// };
